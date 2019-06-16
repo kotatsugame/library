02866f3ce0f35cee67b323d59ff167d4d955f1cb
kotatsugame <takaryou1019@gmail.com>
2019-06-17 02:51:22 +0900

    add

--- BIT.cpp
+++ BIT.cpp
@@ -4,7 +4,7 @@ template<typename T>
 struct BIT{
 	int n;
 	vector<T>bit;
-	BIT(int n_=0,T a=0):n(n_),bit(n_+1,a){}
+	BIT(int n_=0):n(n_),bit(n_+1){}
 	T sum(int i)
 	{
 		T ans=0;
@@ -15,5 +15,14 @@ struct BIT{
 	{
 		if(i==0)return;
 		for(;i<=n;i+=i&-i)bit[i]+=a;
+	}
+	int lower_bound(T k)//k<=sum(ret)
+	{
+		if(k<=0)return 0;
+		int ret=0,i=1;
+		while((i<<1)<=n)i<<=1;
+		for(;i;i>>=1)
+			if(ret+i<=n&&bit[ret+i]<k)k-=bit[ret+=i];
+		return ret+1;
 	}
 };
