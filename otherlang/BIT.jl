11a0644910d1ce7c7d988aa9a0fa1d8345ad45d1
kotatsugame <takaryou1019@gmail.com>
2020-03-06 08:49:44 +0900

    add

--- otherlang/BIT.jl
+++ otherlang/BIT.jl
@@ -17,3 +17,21 @@ function add(bit::BIT,i::Int,a::Int)
 		i+=i&-i
 	end
 end
+function lower_bound(bit::BIT,k::Int)#k<=sum(ret)
+	if k<=0
+		return 0
+	end
+	ret=0
+	i=1
+	while i*2<=bit.n
+		i*=2
+	end
+	while i>0
+		if ret+i<=bit.n&&bit.bit[ret+i]<k
+			k-=bit.bit[ret+i]
+			ret+=i
+		end
+		i>>=1
+	end
+	ret+1
+end
