201ee046f18b1b2bd74a5967ce17cf4c2a6eac45
kotatsugame <takaryou1019@gmail.com>
2020-09-17 14:56:08 +0900

    add test bimatch

---
+++ test/bimatch.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/bipartitematching"
+#include<iostream>
+using namespace std;
+#include"../graph/bimatch.cpp"
+int main()
+{
+	int L,R,M;
+	cin>>L>>R>>M;
+	bimatch P(L+R);
+	for(int i=0;i<M;i++)
+	{
+		int u,v;cin>>u>>v;
+		P.add_edge(u,L+v);
+	}
+	cout<<P.count()<<endl;
+	for(int i=0;i<L;i++)
+	{
+		if(P.match[i]>=L)cout<<i<<" "<<P.match[i]-L<<endl;
+	}
+}
