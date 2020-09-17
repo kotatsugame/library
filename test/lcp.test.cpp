6622c30b755e704786f0f763106497cd5970c1d0
kotatsugame <takaryou1019@gmail.com>
2020-09-17 11:37:06 +0900

    lcp test

---
+++ test/lcp.test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"../string/SA_IS.cpp"
+#include"../string/LCP.cpp"
+using namespace std;
+int main()
+{
+	string s;cin>>s;
+	SA P(s);
+	LCP Q(s);
+	long ans=0;
+	for(int i=0;i<Q.size();i++)ans+=i+1-Q[i];
+	cout<<ans<<endl;
+}
