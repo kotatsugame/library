0db75f61b94bf909484463494f2bd62e7dcb9a38
kotatsugame <takaryou1019@gmail.com>
2020-09-17 16:08:58 +0900

    test DST

---
+++ test/DST.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_sum"
+#include<iostream>
+using namespace std;
+#include"../datastructure/DST.cpp"
+int main()
+{
+	int N,Q;
+	cin>>N>>Q;
+	vector<long>A(N);
+	for(int i=0;i<N;i++)cin>>A[i];
+	DST<long>P(A,[](long a,long b){return a+b;});
+	for(;Q--;)
+	{
+		int l,r;cin>>l>>r;
+		cout<<P.query(l,r)<<endl;
+	}
+}
