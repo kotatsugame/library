dadf48a99ff5e1ef07a01dc7197bf8419d56da86
kotatsugame <takaryou1019@gmail.com>
2020-09-17 10:55:05 +0900

    testlib

---
+++ test/testlib.test.cpp
@@ -0,0 +1,7 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_B"
+#include<iostream>
+#include"../testlib.cpp"
+using namespace std;
+main(){
+	int x;cin>>x;cout<<cube(x)<<endl;
+}
