ce23cfcb1364272be4dc33adb2977439465d28b7
kotatsugame <takaryou1019@gmail.com>
2020-09-17 14:12:20 +0900

    add NTT_anymod test

---
+++ test/NTT_anymod.test.cpp
@@ -0,0 +1,14 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod_1000000007"
+#include<iostream>
+using namespace std;
+#include"math/NTT_anymod.cpp"
+int main()
+{
+	int N,M;
+	cin>>N>>M;
+	vector<int>A(N),B(M);
+	for(int i=0;i<N;i++)cin>>A[i];
+	for(int i=0;i<M;i++)cin>>B[i];
+	vector<int>ans=multiply(A,B,(int)(1e9+7));
+	for(int i=0;i<ans.size();i++)cout<<ans[i]<<(i+1==ans.size()?"\n":" ");
+}
