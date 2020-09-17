bb79a1cfe64b533906298d6e0cb60d3b6e6b4ccc
kotatsugame <takaryou1019@gmail.com>
2020-09-17 16:04:40 +0900

    test UF

---
+++ test/UF.test.cpp
@@ -0,0 +1,16 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+#include<iostream>
+using namespace std;
+#include"../datastructure/UF.cpp"
+int main()
+{
+	int N,Q;
+	cin>>N>>Q;
+	UF uf(N);
+	for(;Q--;)
+	{
+		int t,u,v;cin>>t>>u>>v;
+		if(t==0)uf.unite(u,v);
+		else cout<<uf.same(u,v)<<endl;
+	}
+}
