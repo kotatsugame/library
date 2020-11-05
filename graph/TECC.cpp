b28af7be5233f78c56386ab77faa29487721820b
kotatsugame <takaryou1019@gmail.com>
2020-11-06 00:27:19 +0900

    fix bridge

--- graph/TECC.cpp
+++ graph/TECC.cpp
@@ -1,29 +1,31 @@
 //Two Edge Connected Components require bridge
+#include"../include/bridge.hpp"
 struct TECC:bridge{
 	vector<int>comp;
 	TECC(int n_=0):bridge(n_),comp(n_,-1){}
-	int operator[](int a)const{return comp[a];}
+	int operator[](int i)const{return comp[i];}
 	void dfs(int u,int p,int&k)
 	{
 		comp[u]=p==-1||is_bridge(p,u)?k++:comp[p];
-		for(int&v:G[u])if(comp[v]==-1)dfs(v,u,k);
+		for(int v:G[u])if(comp[v]==-1)dfs(v,u,k);
 	}
 	int build()
 	{
 		bridge::build();
 		int k=0;
-		dfs(0,-1,k);
+		for(int u=0;u<n;u++)if(comp[u]==-1)dfs(u,-1,k);
 		return k;
 	}
 	int build(vector<vector<int> >&H)
 	{
 		int k=build();
 		H.assign(k,vector<int>());
-		for(pair<int,int>&e:bridges)
+		for(int u=0;u<n;u++)
 		{
-			int x=comp[e.first],y=comp[e.second];
-			H[x].push_back(y);
-			H[y].push_back(x);
+			for(int v:G[u])if(comp[u]!=comp[v])
+			{
+				H[comp[u]].push_back(comp[v]);
+			}
 		}
 		return k;
 	}
--- graph/bridge.cpp
+++ graph/bridge.cpp
@@ -1,17 +1,17 @@
 #include<vector>
 #include<algorithm>
 struct bridge{
+	int n;
 	vector<int>ord,low;
 	vector<bool>art;
-	vector<pair<int,int> >bridges;
 	vector<vector<int> >G;
-	bridge(int n_=0):ord(n_,-1),low(n_),art(n_,false),G(n_){}
+	bridge(int n_=0):n(n_),ord(n_,-1),low(n_),art(n_,false),G(n_){}
 	void add_edge(int u,int v)
 	{
 		G[u].push_back(v);
 		G[v].push_back(u);
 	}
-	bool operator[](int a)const{return art[a];}
+	bool operator[](int i)const{return art[i];}
 	bool is_bridge(int a,int b)const
 	{
 		if(ord[a]>ord[b])swap(a,b);
@@ -20,31 +20,36 @@ struct bridge{
 	void dfs(int u,int p,int&k)
 	{
 		low[u]=ord[u]=k++;
-		for(int&v:G[u])
+		bool now=false;
+		int pc=0;
+		for(int v:G[u])
 		{
 			if(ord[v]==-1)
 			{
 				dfs(v,u,k);
 				low[u]=min(low[u],low[v]);
-				art[u]=art[u]|ord[u]<=low[v];
-				if(ord[u]<low[v])bridges.push_back(u<v?make_pair(u,v):make_pair(v,u));
+				now=now||ord[u]<=low[v];
 			}
-			else if(v!=p)
+			else if(v!=p||pc++)
 			{
 				low[u]=min(low[u],ord[v]);
 			}
 		}
+		art[u]=now;
 	}
 	void build()
 	{
-		int k=1,cnt=0;
-		low[0]=ord[0]=0;
-		for(int&v:G[0])if(ord[v]==-1)
+		int k=0;
+		for(int u=0;u<n;u++)if(ord[u]==-1)
 		{
-			dfs(v,0,k);
-			if(ord[0]<low[v])bridges.push_back(make_pair(0,v));
-			cnt++;
+			int cnt=0;
+			low[u]=ord[u]=k++;
+			for(int v:G[u])if(ord[v]==-1)
+			{
+				dfs(v,u,k);
+				cnt++;
+			}
+			if(cnt>=2)art[u]=true;
 		}
-		if(cnt>=2)art[0]=true;
 	}
 };
