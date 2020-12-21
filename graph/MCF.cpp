d2962988a200f992268eb7db5a618772d2b478bc
kotatsugame <takaryou1019@gmail.com>
2020-12-22 03:45:35 +0900

    negative edge MCF

--- graph/MCF.cpp
+++ graph/MCF.cpp
@@ -1,32 +1,60 @@
 //Minimum Cost Flow O(FE log V)
+//Minimum Cost Flow with negative cost O(NE+FE log V)
 #include<algorithm>
 #include<utility>
 #include<vector>
 #include<queue>
 #include<limits>
+#include<cassert>
 template<typename T>
 struct MCF{
 	struct edge{
 		int to,rev,cap;
 		T cost;
 	};
+	int n;
+	bool negedge,ok;
 	vector<vector<edge> >G;
 	vector<T>h,d;
 	vector<int>pv,pe;
-	MCF(int n_=0):G(n_),h(n_,0),d(n_),pv(n_),pe(n_){}
+	MCF(int n_=0):n(n_),negedge(false),G(n_),h(n_),d(n_),pv(n_),pe(n_){}
 	void add_edge(int from,int to,int cap,T cost)
 	{
+		if(cost<0)negedge=true;
 		G[from].push_back({to,(int)G[to].size(),cap,cost});
 		G[to].push_back({from,(int)G[from].size()-1,0,-cost});
 	}
 	T min_cost_flow(int s,int t,int f)//ans or -1
 	{
+		ok=false;
+		if(negedge)
+		{
+			fill(h.begin(),h.end(),numeric_limits<T>::max());
+			h[s]=0;
+			for(int tm=0;tm<n;tm++)
+			{
+				bool ch=false;
+				for(int i=0;i<n;i++)if(h[i]<numeric_limits<T>::max())
+				{
+					for(const edge&e:G[i])
+					{
+						if(e.cap>0&&h[e.to]>h[i]+e.cost)
+						{
+							h[e.to]=h[i]+e.cost;
+							ch=true;
+						}
+					}
+				}
+				if(!ch)break;
+				assert(tm<n-1);//negative cycle
+			}
+		}
 		T ret=0;
 		while(f>0)
 		{
-			priority_queue<pair<T,int>,vector<pair<T,int> >,greater<pair<T,int> > >P;
 			fill(d.begin(),d.end(),numeric_limits<T>::max());
 			d[s]=0;
+			priority_queue<pair<T,int>,vector<pair<T,int> >,greater<pair<T,int> > >P;
 			P.push(make_pair(0,s));
 			while(!P.empty())
 			{
@@ -56,6 +84,8 @@ struct MCF{
 				G[u][G[pv[u]][pe[u]].rev].cap+=d;
 			}
 		}
+		ok=true;
 		return ret;
 	}
+	operator bool()const{return ok;}
 };
---
+++ test/MCF_negative.test.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+#include"MCF.hpp"
+int N,K;
+int A[200];
+int ans=0;
+main()
+{
+	cin>>N>>K;
+	MCF<int>Q(2*N+2);
+	int st=2*N,go=st+1;
+	for(int i=0;i<N;i++)
+	{
+		cin>>A[i];
+		Q.add_edge(st,i,A[i],0);
+	}
+	for(int i=0;i<N;i++)
+	{
+		int B;
+		cin>>B;
+		Q.add_edge(N+i,go,B,0);
+	}
+	for(int i=0;i<N;i++)for(int j=0;j<N;j++)
+	{
+		int P;
+		cin>>P;
+		ans+=P*P;
+		for(int k=1;k<=A[i];k++)Q.add_edge(i,N+j,1,(P-k)*(P-k)-(P-k+1)*(P-k+1));
+	}
+	cout<<ans+Q.min_cost_flow(st,go,K)<<endl;
+}
