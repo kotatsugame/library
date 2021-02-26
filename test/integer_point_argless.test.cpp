842a1e9192c5082ff1c4ef4629b328a4d79ce067
kotatsugame <takaryou1019@gmail.com>
2021-02-26 10:44:27 +0900

    add test argless

---
+++ test/integer_point_argless.test.cpp
@@ -0,0 +1,12 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/sort_points_by_argument"
+using namespace std;
+#include"integer_point.hpp"
+main()
+{
+	int N;
+	cin>>N;
+	vector<Point>P(N);
+	for(int i=0;i<N;i++)cin>>P[i];
+	sort(P.begin(),P.end(),argless);
+	for(int i=0;i<N;i++)cout<<P[i]<<endl;
+}
