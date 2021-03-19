9d08773667c97ee1f2daaced6f1b78674f88e6e2
kotatsugame <takaryou1019@gmail.com>
2021-03-20 08:37:49 +0900

    fix test

--- test/binarytrie_order_of_key.test.cpp
+++ test/binarytrie_order_of_key.test.cpp
@@ -6,10 +6,11 @@ int main()
 {
 	int N,Q;cin>>N>>Q;
 	binarytrie<17>P;
+	const int XOR=(1<<17)-1;
 	for(;Q--;)
 	{
 		int t,x,y;cin>>t>>x>>y;
 		if(t==0)P.add(x,y);
-		else cout<<P.order_of_key(y+1)-P.order_of_key(x)<<endl;
+		else cout<<P.order_of_key((x^XOR)+1,XOR)-P.order_of_key(y^XOR,XOR)<<endl;
 	}
 }
