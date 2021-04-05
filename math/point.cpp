ea8280bd2a2331e75ff24fd61a40eb0bafa59ec8
kotatsugame <takaryou1019@gmail.com>
2021-04-06 01:22:09 +0900

    add area(Circle,Circle)

--- math/point.cpp
+++ math/point.cpp
@@ -1,19 +1,21 @@
-#define _USE_MATH_DEFINES
+using namespace std;
 #include<iostream>
 #include<algorithm>
 #include<vector>
 #include<cmath>
 #include<iomanip>
-const double EPS=1e-10;
-bool eq(double a,double b){return abs(a-b)<EPS;}
+using Real=double;
+const Real EPS=1e-10;
+const Real PI=acos((Real)-1);
+bool eq(Real a,Real b){return abs(a-b)<EPS;}
 struct Point{
-	double x,y;
-	Point(double x_=0,double y_=0):x(x_),y(y_){}
+	Real x,y;
+	Point(Real x_=0,Real y_=0):x(x_),y(y_){}
 	Point operator-()const{return Point(-x,-y);}
 	Point operator+(const Point&p)const{return Point(x+p.x,y+p.y);}
 	Point operator-(const Point&p)const{return Point(x-p.x,y-p.y);}
-	Point operator*(const double k)const{return Point(x*k,y*k);}
-	Point operator/(const double k)const{return Point(x/k,y/k);}
+	Point operator*(const Real k)const{return Point(x*k,y*k);}
+	Point operator/(const Real k)const{return Point(x/k,y/k);}
 	bool operator<(const Point&p)const{return eq(x,p.x)?y<p.y:x<p.x;}
 	bool operator==(const Point&p)const{return eq(x,p.x)&&eq(y,p.y);}
 };
@@ -28,25 +30,25 @@ struct Segment:Line{
 };
 struct Circle{
 	Point o;
-	double r;
-	Circle(Point o_=Point(),double r_=0):o(o_),r(r_){}
+	Real r;
+	Circle(Point o_=Point(),Real r_=0):o(o_),r(r_){}
 };
 using Polygon=vector<Point>;
 //function list begin
 Point vec(const Line&);
-double norm(const Point&);
-double norm(const Line&);
-double abs(const Point&);
-double abs(const Line&);
-double arg(const Point&);
-double arg(const Line&);
-double arg(const Point&,const Point&,const Point&);// /_abc,[0,pi]
+Real norm(const Point&);
+Real norm(const Line&);
+Real abs(const Point&);
+Real abs(const Line&);
+Real arg(const Point&);
+Real arg(const Line&);
+Real arg(const Point&,const Point&,const Point&);// /_abc,[0,pi]
 int argtype(const Point&);////(-pi,0]->-1,(0,pi]->1,(0,0)->0
 bool argless(const Point&,const Point&);//sorting points with arg
-double dot(const Point&,const Point&);
-double cross(const Point&,const Point&);
-Point polar(const double,const double);
-Point rotate(const Point&,const double);
+Real dot(const Point&,const Point&);
+Real cross(const Point&,const Point&);
+Point polar(const Real,const Real);
+Point rotate(const Point&,const Real);
 enum{ONLINE_FRONT=-2,CLOCKWISE=-1,ON_SEGMENT=0,COUNTER_CLOCKWISE=1,ONLINE_BACK=2};
 int ccw(const Point&,const Point&);
 int ccw(const Point&,const Point&,const Point&);
@@ -66,17 +68,17 @@ int intersect(const Circle&,const Line&);//count contacts
 int intersect(const Circle&,const Segment&);//count contacts
 bool intersect(const Circle&,const Circle&);
 int count_tangent(const Circle&,const Circle&);//count common tangents
-double distance(const Point&,const Point&);
-double distance(const Line&,const Point&);
-double distance(const Line&,const Line&);
-double distance(const Segment&,const Point&);
-double distance(const Segment&,const Segment&);
-double distance(const Line&,const Segment&);
-double distance(const Segment&,const Line&);
-double distance(const Circle&,const Point&);
-double distance(const Circle&,const Line&);
-double distance(const Circle&,const Segment&);
-double distance(const Circle&,const Circle&);
+Real distance(const Point&,const Point&);
+Real distance(const Line&,const Point&);
+Real distance(const Line&,const Line&);
+Real distance(const Segment&,const Point&);
+Real distance(const Segment&,const Segment&);
+Real distance(const Line&,const Segment&);
+Real distance(const Segment&,const Line&);
+Real distance(const Circle&,const Point&);
+Real distance(const Circle&,const Line&);
+Real distance(const Circle&,const Segment&);
+Real distance(const Circle&,const Circle&);
 Point projection(const Line&,const Point&);
 Point reflection(const Line&,const Point&);
 Point crosspoint(const Line&,const Line&);
@@ -93,22 +95,24 @@ int contain(const Circle&,const Point&);
 int contain(const Circle&,const Segment&);
 int convex_contain(const Polygon&,const Point&);//O(log |P|)
 Polygon convex_cut(const Polygon&,const Line&);
-double diameter(Polygon);
-double area(const Polygon&);
-double area(const Polygon&,const Line&);
-double area(const Polygon&,const Circle&);
+Real diameter(Polygon);
+Real area(const Polygon&);
+Real area(const Polygon&,const Line&);
+Real area(const Polygon&,const Circle&);
+Real area(const Circle&);
+Real area(const Circle&,const Ciecle&);
 //function list end
 Point vec(const Line&s){return s.p2-s.p1;}
-double norm(const Point&p){return p.x*p.x+p.y*p.y;}
-double norm(const Line&s){return norm(vec(s));}
-double abs(const Point&p){return hypot(p.x,p.y);}
-double abs(const Line&s){return abs(vec(s));}
-double arg(const Point&p){return atan2(p.y,p.x);}
-double arg(const Line&s){return arg(vec(s));}
-double arg(const Point&a,const Point&b,const Point&c){
-	double A=arg(a-b),B=arg(c-b);
-	double theta=abs(A-B);
-	return min(theta,2*M_PI-theta);
+Real norm(const Point&p){return p.x*p.x+p.y*p.y;}
+Real norm(const Line&s){return norm(vec(s));}
+Real abs(const Point&p){return hypot(p.x,p.y);}
+Real abs(const Line&s){return abs(vec(s));}
+Real arg(const Point&p){return atan2(p.y,p.x);}
+Real arg(const Line&s){return arg(vec(s));}
+Real arg(const Point&a,const Point&b,const Point&c){
+	Real A=arg(a-b),B=arg(c-b);
+	Real theta=abs(A-B);
+	return min(theta,2*PI-theta);
 }
 int argtype(const Point&a)
 {
@@ -119,10 +123,10 @@ bool argless(const Point&a,const Point&b)
 	int at=argtype(a),bt=argtype(b);
 	return at!=bt?at<bt:ccw(a,b)>0;
 }
-double dot(const Point&a,const Point&b){return a.x*b.x+a.y*b.y;}
-double cross(const Point&a,const Point&b){return a.x*b.y-a.y*b.x;}
-Point polar(const double r,const double theta){return Point(cos(theta),sin(theta))*r;}
-Point rotate(const Point&p,const double theta){
+Real dot(const Point&a,const Point&b){return a.x*b.x+a.y*b.y;}
+Real cross(const Point&a,const Point&b){return a.x*b.y-a.y*b.x;}
+Point polar(const Real r,const Real theta){return Point(cos(theta),sin(theta))*r;}
+Point rotate(const Point&p,const Real theta){
 	return Point(p.x*cos(theta)-p.y*sin(theta),p.x*sin(theta)+p.y*cos(theta));
 }
 int ccw(const Point&a,const Point&b)
@@ -151,12 +155,12 @@ bool intersect(const Line&s,const Segment&t){
 bool intersect(const Segment&s,const Line&t){return intersect(t,s);}
 bool intersect(const Circle&c,const Point&p){return eq(distance(c.o,p),c.r);}
 int intersect(const Circle&c,const Line&s){
-	double d=distance(s,c.o);
+	Real d=distance(s,c.o);
 	return eq(d,c.r)?1:d<c.r?2:0;
 }
 int intersect(const Circle&c,const Segment&s){
 	Point h=projection(s,c.o);
-	double d1=distance(c.o,s.p1),d2=distance(c.o,s.p2);
+	Real d1=distance(c.o,s.p1),d2=distance(c.o,s.p2);
 	return distance(c.o,h)>c.r+EPS?0
 		:d1<c.r-EPS&&d2<c.r-EPS?0
 		:(d1<c.r-EPS&&d2>c.r-EPS)||(d1>c.r-EPS&&d2<c.r-EPS)?1
@@ -168,44 +172,44 @@ bool intersect(const Circle&a,const Circle&b){
 	return 1<=c&&c<=3;
 }
 int count_tangent(const Circle&a,const Circle&b){
-	double d=distance(a.o,b.o);
+	Real d=distance(a.o,b.o);
 	return eq(d,a.r+b.r)?3:d>a.r+b.r?4:eq(d,abs(a.r-b.r))?1:d>abs(a.r-b.r)?2:0;
 }
-double distance(const Point&a,const Point&b){return abs(a-b);}
-double distance(const Line&s,const Point&p){return distance(p,projection(s,p));}
-double distance(const Line&s,const Line&t){return intersect(s,t)?0:distance(s,t.p1);}
-double distance(const Segment&s,const Point&p){
+Real distance(const Point&a,const Point&b){return abs(a-b);}
+Real distance(const Line&s,const Point&p){return distance(p,projection(s,p));}
+Real distance(const Line&s,const Line&t){return intersect(s,t)?0:distance(s,t.p1);}
+Real distance(const Segment&s,const Point&p){
 	return distance(p,
 		dot(vec(s),p-s.p1)<0?s.p1
 		:dot(-vec(s),p-s.p2)<0?s.p2
 		:projection(s,p)
 	);
 }
-double distance(const Segment&s,const Segment&t){
+Real distance(const Segment&s,const Segment&t){
 	return intersect(s,t)?0:min({
 		distance(s,t.p1),distance(s,t.p2),
 		distance(t,s.p1),distance(t,s.p2)
 	});
 }
-double distance(const Line&s,const Segment&t){
+Real distance(const Line&s,const Segment&t){
 	return intersect(s,t)?0:min(distance(s,t.p1),distance(s,t.p2));
 }
-double distance(const Segment&s,const Line&t){return distance(t,s);}
-double distance(const Circle&c,const Point&p){return abs(distance(c.o,p)-c.r);}
-double distance(const Circle&c,const Line&s){return max(distance(s,c.o)-c.r,0.);}
-double distance(const Circle&c,const Segment&s){
+Real distance(const Segment&s,const Line&t){return distance(t,s);}
+Real distance(const Circle&c,const Point&p){return abs(distance(c.o,p)-c.r);}
+Real distance(const Circle&c,const Line&s){return max(distance(s,c.o)-c.r,(Real)0);}
+Real distance(const Circle&c,const Segment&s){
 	return intersect(c,s)?0
 		:contain(c,s)?c.r-max(distance(c.o,s.p1),distance(c.o,s.p2))
 		:distance(s,c.o)-c.r;
 }
-double distance(const Circle&a,const Circle&b){return max(distance(a.o,b.o)-a.r-b.r,0.);}
+Real distance(const Circle&a,const Circle&b){return max(distance(a.o,b.o)-a.r-b.r,(Real)0);}
 Point projection(const Line&s,const Point&p){
 	return s.p1+vec(s)*dot(p-s.p1,vec(s))/norm(s);
 }
 Point reflection(const Line&s,const Point&p){return projection(s,p)*2-p;}
 Point crosspoint(const Line&s,const Line&t){
-	double d1=cross(vec(s),t.p1-s.p1);
-	double d2=-cross(vec(s),t.p2-s.p1);
+	Real d1=cross(vec(s),t.p1-s.p1);
+	Real d2=-cross(vec(s),t.p2-s.p1);
 	return t.p1+vec(t)*(d1/(d1+d2));
 }
 pair<Point,Point>crosspoint(const Circle&c,const Line&s){
@@ -220,9 +224,9 @@ pair<Point,Point>crosspoint(const Circle&c,const Segment&s){
 		:make_pair(p.second,p.second);
 }
 pair<Point,Point>crosspoint(const Circle&a,const Circle&b){
-	double d=distance(a.o,b.o);
-	double alpha=acos((a.r*a.r+d*d-b.r*b.r)/(2*a.r*d));
-	double theta=arg(b.o-a.o);
+	Real d=distance(a.o,b.o);
+	Real alpha=acos((a.r*a.r+d*d-b.r*b.r)/(2*a.r*d));
+	Real theta=arg(b.o-a.o);
 	return minmax(a.o+polar(a.r,theta+alpha),a.o+polar(a.r,theta-alpha));
 }
 pair<Point,Point>tangent(const Circle&c,const Point&p){
@@ -230,12 +234,12 @@ pair<Point,Point>tangent(const Circle&c,const Point&p){
 }
 vector<Line>tangent(const Circle&a,const Circle&b){
 	vector<Line>ret;
-	double g=distance(a.o,b.o);
+	Real g=distance(a.o,b.o);
 	if(eq(g,0))return ret;
 	Point u=(b.o-a.o)/g;
-	Point v=rotate(u,M_PI/2);
+	Point v=rotate(u,PI/2);
 	for(int s:{-1,1}){
-		double h=(a.r+b.r*s)/g;
+		Real h=(a.r+b.r*s)/g;
 		if(eq(h*h,1))ret.emplace_back(a.o+(h>0?u:-u)*a.r,a.o+(h>0?u:-u)*a.r+v);
 		else if(1-h*h>0){
 			Point U=u*h,V=v*sqrt(1-h*h);
@@ -298,11 +302,11 @@ int contain(const Polygon&P,const Point&p){
 	return in?IN:OUT;
 }
 int contain(const Circle&c,const Point&p){
-	double d=distance(c.o,p);
+	Real d=distance(c.o,p);
 	return eq(d,c.r)?ON:d<c.r?IN:OUT;
 }
 int contain(const Circle&c,const Segment&s){
-	double d1=distance(c.o,s.p1),d2=distance(c.o,s.p2);
+	Real d1=distance(c.o,s.p1),d2=distance(c.o,s.p2);
 	return d1<c.r+EPS&&d2<c.r+EPS?eq(d1,c.r)||eq(d2,c.r)?ON:IN:OUT;
 }
 int convex_contain(const Polygon&P,const Point&p)
@@ -342,14 +346,14 @@ Polygon convex_cut(const Polygon&P,const Line&s){
 	}
 	return ret;
 }
-double diameter(Polygon P){
+Real diameter(Polygon P){
 	if(!is_convex(P))P=convex_hull(P);
 	int mi=0,Mi=0;
 	for(int i=1;i<P.size();i++){
 		if(P[i].x<P[mi].x)mi=i;
 		if(P[i].x>P[Mi].x)Mi=i;
 	}
-	double ret=0;
+	Real ret=0;
 	int sm=mi,sM=Mi;
 	while(mi!=sM||Mi!=sm){
 		ret=max(ret,norm(P[mi]-P[Mi]));
@@ -358,22 +362,22 @@ double diameter(Polygon P){
 	}
 	return sqrt(ret);
 }
-double area(const Polygon&P){
-	double ret=0;
+Real area(const Polygon&P){
+	Real ret=0;
 	for(int i=0;i<P.size();i++)ret+=cross(P[i],P[(i+1)%P.size()]);
 	return ret/2;
 }
-double area(const Polygon&P,const Line&s){return area(convex_cut(P,s));}
-double area(const Polygon&P,const Circle&c){
-	double ret=0;
+Real area(const Polygon&P,const Line&s){return area(convex_cut(P,s));}
+Real area(const Polygon&P,const Circle&c){
+	Real ret=0;
 	for(int i=0;i<P.size();i++)
 	{
 		Segment s(P[i],P[(i+1)%P.size()]);
 		if(contain(c,s))ret+=cross(s.p1-c.o,s.p2-c.o);
 		else if(!intersect(c,s)){
-			double a=arg(s.p2-c.o)-arg(s.p1-c.o);
-			if(a>M_PI)a-=2*M_PI;
-			if(a<-M_PI)a+=2*M_PI;
+			Real a=arg(s.p2-c.o)-arg(s.p1-c.o);
+			if(a>PI)a-=2*PI;
+			if(a<-PI)a+=2*PI;
 			ret+=c.r*c.r*a;
 		}
 		else
@@ -386,13 +390,36 @@ double area(const Polygon&P,const Circle&c){
 				Segment t(tmp[j],tmp[j+1]);
 				if(contain(c,t))ret+=cross(t.p1-c.o,t.p2-c.o);
 				else{
-					double a=arg(t.p2-c.o)-arg(t.p1-c.o);
-					if(a>M_PI)a-=2*M_PI;
-					if(a<-M_PI)a+=2*M_PI;
+					Real a=arg(t.p2-c.o)-arg(t.p1-c.o);
+					if(a>PI)a-=2*PI;
+					if(a<-PI)a+=2*PI;
 					ret+=c.r*c.r*a;
 				}
 			}
 		}
 	}
 	return ret/2;
+}
+Real area(const Circle&c){return PI*c.r*c.r;}
+Real area(const Circle&a,const Circle&b)
+{
+	int c=count_tangent(a,b);
+	if(c>=3)return 0;
+	else if(c<=1)return area(a.r<b.r?a:b);
+	else
+	{
+		Real d=distance(a.o,b.o);
+		Real ret=0;
+		{
+			Real alpha=acos((a.r*a.r+d*d-b.r*b.r)/(2*a.r*d));
+			ret+=alpha*a.r*a.r;
+			ret-=sin(alpha*2)*a.r*a.r/2;
+		}
+		{
+			Real alpha=acos((b.r*b.r+d*d-a.r*a.r)/(2*b.r*d));
+			ret+=alpha*b.r*b.r;
+			ret-=sin(alpha*2)*b.r*b.r/2;
+		}
+		return ret;
+	}
 }
