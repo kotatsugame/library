e0ed87c574d7fa64d8ba5f4927c0e5ec69d11c06
kotatsugame <takaryou1019@gmail.com>
2022-04-16 06:34:11 +0900

    add invfac to combination

--- math/combination.cpp
+++ math/combination.cpp
@@ -1,8 +1,8 @@
 #include<vector>
 template<typename T>
 struct combination{
-	vector<T>fac,invfac;
-	combination(size_t N=0):fac(1,1),invfac(1,1)
+	vector<T>fac,ifac;
+	combination(size_t N=0):fac(1,1),ifac(1,1)
 	{
 		make_table(N);
 	}
@@ -12,27 +12,32 @@ struct combination{
 		size_t now=fac.size();
 		N=max(N,now*2);
 		fac.resize(N+1);
-		invfac.resize(N+1);
+		ifac.resize(N+1);
 		for(size_t i=now;i<=N;i++)fac[i]=fac[i-1]*i;
-		invfac[N]=1/fac[N];
-		for(size_t i=N;i-->now;)invfac[i]=invfac[i+1]*(i+1);
+		ifac[N]=1/fac[N];
+		for(size_t i=N;i-->now;)ifac[i]=ifac[i+1]*(i+1);
 	}
 	T factorial(size_t n)
 	{
 		make_table(n);
 		return fac[n];
 	}
+	T invfac(size_t n)
+	{
+		make_table(n);
+		return ifac[n];
+	}
 	T P(size_t n,size_t k)
 	{
 		if(n<k)return 0;
 		make_table(n);
-		return fac[n]*invfac[n-k];
+		return fac[n]*ifac[n-k];
 	}
 	T C(size_t n,size_t k)
 	{
 		if(n<k)return 0;
 		make_table(n);
-		return fac[n]*invfac[n-k]*invfac[k];
+		return fac[n]*ifac[n-k]*ifac[k];
 	}
 	T H(size_t n,size_t k)
 	{
