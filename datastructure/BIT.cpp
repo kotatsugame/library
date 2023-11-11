da503f238ec6c12bc5fbc9848b18dd5cf60c3982
kotatsugame <takaryou1019@gmail.com>
2023-11-12 08:29:25 +0900

    0-indexed BIT

--- datastructure/BIT.cpp
+++ datastructure/BIT.cpp
@@ -1,28 +1,38 @@
-//1-indexed
+//0-indexed
 #include<vector>
+#include<cassert>
 template<typename T>
 struct BIT{
-	int n;
+	int N;
 	vector<T>bit;
-	BIT(int n_=0):n(n_),bit(n_+1){}
+	BIT(int N_=0):N(N_),bit(N_){}
 	T sum(int i)
 	{
+		assert(0<=i&&i<=N);
 		T ans=0;
-		for(;i>0;i-=i&-i)ans+=bit[i];
+		for(;i>0;i-=i&-i)ans+=bit[i-1];
 		return ans;
 	}
 	void add(int i,T a)
 	{
-		if(i==0)return;
-		for(;i<=n;i+=i&-i)bit[i]+=a;
+		assert(0<=i&&i<N);
+		i++;
+		for(;i<=N;i+=i&-i)bit[i-1]+=a;
 	}
 	int lower_bound(T k)//k<=sum(ret)
 	{
 		if(k<=0)return 0;
 		int ret=0,i=1;
-		while((i<<1)<=n)i<<=1;
+		while((i<<1)<=N)i<<=1;
 		for(;i;i>>=1)
-			if(ret+i<=n&&bit[ret+i]<k)k-=bit[ret+=i];
+		{
+			if(ret+i<=N&&bit[ret+i-1]<k)
+			{
+				ret+=i;
+				k-=bit[ret-1];
+			}
+		}
 		return ret+1;
+		//ret+1 == N+1 <=> sum<k
 	}
 };
--- test/BIT.test.cpp
+++ test/BIT.test.cpp
@@ -9,13 +9,13 @@ int main()
 	BIT<long>P(N);
 	for(int i=0;i<N;i++)
 	{
-		long A;cin>>A;
-		P.add(i+1,A);
+		int A;cin>>A;
+		P.add(i,A);
 	}
 	for(;Q--;)
 	{
 		int t,l,r;cin>>t>>l>>r;
-		if(t==0)P.add(l+1,r);
-		else cout<<P.sum(r)-P.sum(l)<<endl;
+		if(t==0)P.add(l,r);
+		else cout<<P.sum(r)-P.sum(l)<<"\n";
 	}
 }
